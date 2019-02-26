87068d1f40ad6d2486262b678237b681807dbc7e
Alejandro Munoz <amunoz797@gmail.com>
2019-02-26 02:08:10 -0500

    Fixed github pages deployer

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,8 @@
 
 GIT_DEPLOY_REPO=${GIT_DEPLOY_REPO:-$(node -p -e "require('./package.json').repository.url")}
 
-cd documentation && \
+cd docs && \
 
-rm -R .git
 git init && \
 
 git config user.name "Travis CI" && \
@@ -13,4 +12,4 @@ git config user.email "github@travis-ci.org" && \
 git add . && \
 git commit -m "Deploy to GitHub Pages" && \
 
-git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
