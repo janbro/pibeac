5eb1dd4a40756401a3ee392e07758a053329bba7
Alejandro Munoz <amunoz797@gmail.com>
2019-02-25 22:27:23 -0500

    Updated mongo credentials to be deployer friendly

--- config/config.js
+++ config/config.js
@@ -1,8 +1,16 @@
-var SECRETS = require('../secrets.json');
+try {
+    var SECRETS = require('../secrets.json');
+    var CREDS = SECRETS.MONGO_CREDENTIALS;
+}
+catch(e) {
+    if (e.code !== "MODULE_NOT_FOUND")
+        throw e;
+    var CREDS = process.env.MONGO_CREDENTIALS;
+}
 
 module.exports = {
     db: {
-      uri: 'mongodb://' + SECRETS.MONGO_CREDENTIALS + '@ds151805.mlab.com:51805/beamhub', //place the URI of your mongo database here.
+      uri: 'mongodb://' + CREDS + '@ds151805.mlab.com:51805/beamhub', //place the URI of your mongo database here.
     },
     port: process.env.PORT || 8080
 };
